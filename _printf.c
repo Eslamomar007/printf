de3e11839a8230a12a507a89a362c4ed228e6f2a
Eslamomar007 <eslamoomar007@gmail.com>
2024-02-11 13:52:01 +0200

    done

--- _printf.c
+++ _printf.c
@@ -23,6 +23,7 @@ int _printf(const char *format, ...)
 	{
 		while (format[i] == '%')
 		{
+			format++;
 			if (format[i] == 'c')
 			{
 				int var_char = va_arg(list, int);
@@ -36,13 +37,17 @@ int _printf(const char *format, ...)
 					printed_num += _putchar(*var_string);
 			}
 			if (format[i] == 'd')
+			{
 				printed_num += _int_print(list);
-
-			if (format[i] == 37)
-				printed_num += _putchar('%');
-			j++;
+				format++;
+			}
+			if (format[i] == "%"[0])
+			{
+				printed_num += _putchar(37);
+			}
 		}
 		printed_num += _putchar(format[i]);
+		format++;
 	}
 	va_end(list);
 	return (printed_num);
